f1cc9ec11c28b843cadf5a4461c20bbcb33ce66a
Azmo Rinsler <azmo-rinsler@idexx.com>
2025-06-16 09:02:01 -0400

    Initial Commit

---
+++ src/main/java/ADOTLambdaHandler.java
@@ -0,0 +1,33 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Random;
+
+@SuppressWarnings("unused") // AWS Lambda uses this handler
+public class ADOTLambdaHandler implements RequestStreamHandler {
+
+    Logger logger = LoggerFactory.getLogger(ADOTLambdaHandler.class);
+    @Override
+    public void handleRequest(final InputStream inputStream, final OutputStream outputStream, final Context context) {
+        try {
+            logger.trace("Lambda function received request");
+
+            // Simulate doing <stuff> with a random delay
+            var delay = new Random().nextInt(1000, 6000);
+            logger.info("Sleeping for {} seconds to simulate doing <stuff>", delay / 1000.0);
+            Thread.sleep(delay);
+            logger.info("Finished doing <stuff>");
+
+            logger.trace("Lambda function completed successfully");
+        }
+        catch (InterruptedException e) {
+            logger.warn("Lambda function was interrupted");
+            logger.error(e.getMessage());
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/ADOTLambdaHandlerTest.java
@@ -0,0 +1,25 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
+import static org.mockito.Mockito.mock;
+
+public class ADOTLambdaHandlerTest {
+    @Test
+    public void testHandleRequest() {
+        var inputMessage = """
+                {
+                    "key1": "value1",
+                    "key2": "value2"
+                }
+                """;
+        var inputStream = new ByteArrayInputStream(inputMessage.getBytes(StandardCharsets.UTF_8));
+        var outputStream = new ByteArrayOutputStream();
+        var context = mock(Context.class);
+        var handler = new ADOTLambdaHandler();
+        handler.handleRequest(inputStream, outputStream, context);
+    }
+}
