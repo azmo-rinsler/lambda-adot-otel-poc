c4747b0c9277e9adc231baf0e1d093d852ff9bf3
Azmo Rinsler <azmo-rinsler@idexx.com>
2025-06-19 15:13:05 -0400

    trying to get otel into mdc context

--- src/main/java/ADOTLambdaHandler.java
+++ src/main/java/ADOTLambdaHandler.java
@@ -3,31 +3,43 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.context.Scope;
+import org.slf4j.MDC;
+
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Random;
 
-@SuppressWarnings("unused") // AWS Lambda uses this handler
+@SuppressWarnings("unused")
 public class ADOTLambdaHandler implements RequestStreamHandler {
-    @Override // this is the handler method for the "normal" RequestStreamHandler
+    private static final Logger logger = LoggerFactory.getLogger(ADOTLambdaHandler.class);
+
+    @Override
     public void handleRequest(final InputStream inputStream, final OutputStream outputStream, final Context context) {
-        // Using SLF4J for logging (imported as a transitive dependency from logback-classic)
-        Logger logger = LoggerFactory.getLogger(ADOTLambdaHandler.class);
-        try {
+        // Start or fetch current span
+        Span span = Span.current();
+
+        try (Scope scope = span.makeCurrent()) {
+            // Inject trace/span into MDC for log correlation
+            MDC.put("trace_id", span.getSpanContext().getTraceId());
+            MDC.put("span_id", span.getSpanContext().getSpanId());
+
             logger.trace("Lambda function received request");
 
-            // Simulate doing <stuff> with a random delay
-            var delay = new Random().nextInt(1000, 6000);
-            logger.info("Sleeping for {} seconds to simulate doing things and stuff", delay / 1000.0);
+            int delay = new Random().nextInt(1000, 6000);
+            logger.info("Sleeping for {} seconds to simulate doing things", delay / 1000.0);
             Thread.sleep(delay);
             logger.info("Finished doing things and stuff");
-
             logger.trace("Lambda function completed successfully");
-        }
-        catch (InterruptedException e) {
+
+        } catch (InterruptedException e) {
             logger.warn("Lambda function was interrupted");
-            logger.error(e.getMessage());
+            logger.error("Exception: ", e);
             throw new RuntimeException(e);
+        } finally {
+            // Always clear MDC to avoid contaminating future logs in the same container
+            MDC.clear();
         }
     }
 }
