117dbd73ef8bdb323685022fbeb17bd4165463f9
Azmo Rinsler <azmo-rinsler@idexx.com>
2025-06-19 16:06:30 -0400

    adding request id instead of trace and span ids to logger output

--- src/main/java/ADOTLambdaHandler.java
+++ src/main/java/ADOTLambdaHandler.java
@@ -3,8 +3,6 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.context.Scope;
 import org.slf4j.MDC;
 
 import java.io.InputStream;
@@ -17,22 +15,14 @@ public class ADOTLambdaHandler implements RequestStreamHandler {
 
     @Override
     public void handleRequest(final InputStream inputStream, final OutputStream outputStream, final Context context) {
-        // Start or fetch current span
-        Span span = Span.current();
-
-        try (Scope scope = span.makeCurrent()) {
-            // Inject trace/span into MDC for log correlation
-            MDC.put("trace_id", span.getSpanContext().getTraceId());
-            MDC.put("span_id", span.getSpanContext().getSpanId());
-
+        try {
+            MDC.put("RequestId", context.getAwsRequestId());
             logger.trace("Lambda function received request");
-
             int delay = new Random().nextInt(1000, 6000);
             logger.info("Sleeping for {} seconds to simulate doing things", delay / 1000.0);
             Thread.sleep(delay);
             logger.info("Finished doing things and stuff");
             logger.trace("Lambda function completed successfully");
-
         } catch (InterruptedException e) {
             logger.warn("Lambda function was interrupted");
             logger.error("Exception: ", e);
